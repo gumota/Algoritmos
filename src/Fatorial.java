ced81416cbc04f4312c8de2b720112beaf2e023b
Gustavo Mota <gustavo.omota@hotmail.com>
2020-03-05 23:04:05 -0300

    First algoritmo

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 
 public class Fatorial {
 
-	int fat = 1;
+	private static Scanner in;
+
+	public static void main(String[] args) {
+
+		long fat = 1;
+		in = new Scanner(System.in);
+
+		System.out.println("Digite um número e obtenha o seu fatorial:");
+		int num = in.nextInt();
+		int numero = num;
+
+		for (; 1 <= num; num--) {
+			fat = fat * num;
+
+		}
+		System.out.println("O fatorial de " + numero + " é igual a " + fat);
+
+	}
 
 }
