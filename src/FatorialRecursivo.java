b2d95ab89bd8f79fad456931ac23748a7f89b38b
Gustavo Mota <gustavo.omota@hotmail.com>
2020-03-06 09:29:56 -0300

    Ajustes

--- src/FatorialRecursivo.java
+++ src/FatorialRecursivo.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
 
 public class FatorialRecursivo {
+
+	private static Scanner in;
+
+	public static void main(String[] args) {
+
+		in = new Scanner(System.in);
+
+		System.out.println("Digite um número e obtenha seu fatorial:");
+
+		int num = in.nextInt();
+
+		System.out.println(calculaFatorial(num));
+
+	}
+
+	private static int calculaFatorial(int num) {
+
+		if (num <= 1) {
+
+			return 1;
+
+		} else {
+
+			return calculaFatorial(num - 1) * num;
+		}
+
+	}
 
 }
